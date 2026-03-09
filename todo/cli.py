ddb1476e31bff3064e498ad73fb2a66351a2cf05
alex <0alexgorbachev0@gmail.com>
2026-03-09 22:00:48 +0000

    initial commit

---
+++ todo/cli.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import shutil
+import random
+import os
+import sys
+
+def add():
+    pass
+def remove():
+    pass
+def view():
+    pass
+def pickrandomtask():
+    pass
+
+def main():
+    commands = {
+        "a": add,
+        "rm":remove,
+        "v":view,
+        "rd":pickrandomtask
+    }
+main()
