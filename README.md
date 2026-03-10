a5801ae539e56b83b9b446c35b7b109e485f4ff4
alex <0alexgorbachev0@gmail.com>
2026-03-10 23:40:22 +0000

    Version 1.0 - released with 4 actual functions and one function to help if the user forgets commands

---
+++ README.md
@@ -0,0 +1,30 @@
+## todo
+A simple terminal based todo app to help keep track of tasks
+
+## Installation
+
+```bash
+# Install with pipx (recommended)
+pipx install git+https://github.com/demtellme/todo.git
+
+# Or with pip
+pip install --user git+https://github.com/demtellme/todo.git
+```
+
+## Uninstall Command
+```bash
+pipx uninstall todo
+```
+## How to use
+```bash
+#arguments only if necessary
+todo <command> <argument one> 
+```
+ ## Commands:
+    h - displays commands if you forget :)
+    v  - view all tasks
+    a "<task>" - add an item to the todo list, has an option if you dont provide an argument
+    rm <task> - removes a task, has an option if you dont provide an argument
+    rd - if youre feeling indecisive it picks a random task for you to complete
+
+Made By Alex G
--- todo/cli.py
+++ todo/cli.py
@@ -1,23 +1,66 @@
 #!/usr/bin/env python3
-import shutil
 import random
 import os
 import sys
 
-def add():
-    pass
-def remove():
-    pass
+BASEDIR = os.path.expanduser("~")
+TODOLIST = os.path.join(BASEDIR, ".todolist.txt")
+
+def help():
+    print(""" a "<task>" - adds a task
+        v - veiws what tasks need to be done
+        rm "<task>" - removes a task
+        rd - picks a random task for you to do if you dont know what should be done
+        """)
+
+
+def add(tasktoadd=None):
+    if tasktoadd is None:
+        tasktoadd = input("What task would you want to add to your list: ")
+    open(TODOLIST, "a+").write(tasktoadd + "\n")
+
+
+def remove(tasktoremove=None):
+    lines = open(TODOLIST, "r").readlines()
+    if tasktoremove is None:
+        for i, line in enumerate(lines):
+            print(f"{i}: {line}")
+        indextoremove = int(input("what is the number of the line you want removed: "))
+        del lines[indextoremove]
+    else:
+        if tasktoremove + "\n" in lines:
+            lines.remove(tasktoremove + "\n")
+    open(TODOLIST, "w").writelines(lines)
+
 def view():
-    pass
+    items = open(TODOLIST, "r").readlines()
+    for item in items:
+        print(item)
+
 def pickrandomtask():
-    pass
+    lines = open(TODOLIST, "r").readlines()
+    randomtask = random.choice(lines)
+    print(f"{randomtask} - is the random task selected")
 
 def main():
     commands = {
+        "h":help,
         "a": add,
         "rm":remove,
         "v":view,
         "rd":pickrandomtask
     }
-main()
+
+    argumentone = ""
+
+    command = sys.argv[1]
+    if len(sys.argv) == 3:
+        argumentone = sys.argv[2]
+
+    if command in commands:
+         if argumentone:
+             commands[command](argumentone)
+         else:
+             commands[command]()
+    else:
+         print("Invalid command, run todo h for help")
